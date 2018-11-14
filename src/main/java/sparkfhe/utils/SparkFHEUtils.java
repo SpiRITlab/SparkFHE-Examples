57a5d572ca31dbcd9a36cc258ac03b75e9bce82b
dboss3 <dboss3@users.noreply.github.com>
2018-11-14 03:15:41 -0500

    Add swig jar as dependency

--- src/main/java/sparkfhe/utils/SparkFHEUtils.java
+++ src/main/java/sparkfhe/utils/SparkFHEUtils.java
@@ -7,7 +7,7 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
+import sparkfhe.SparkFHE;
 
 /**
  * Hello world!
@@ -17,27 +17,31 @@ public class SparkFHEUtils {
     public static final String RECORDS_PATH = System.getProperty("user.dir") + "/../../../bin/records";
     private static final JsonNodeFactory factory = JsonNodeFactory.instance;
 
-    public static String loadCtxt(String path) {
+    public static String loadCtxt(String ctxtFilePath) {
         try
         {
-            String ctxt_full = new String(Files.readAllBytes(Paths.get(path)));
+            String ctxt_full = new String(Files.readAllBytes(Paths.get(ctxtFilePath)));
             final ObjectNode ctxt_node = new ObjectMapper().readValue(ctxt_full, ObjectNode.class);
             return ctxt_node.get("ctxt").textValue();
         }
         catch (IOException e)
         {
-            System.err.println("Could not load ciphertext from file: " + path);
+            System.err.println("Could not load ciphertext from file: " + ctxtFilePath);
             return null;
         }
     }
 
-    public static void storeCtxt(String ctxt, String path) {
+    public static void storeCtxt(String ctxt, String ctxtFilePath) {
         ObjectNode ctxtObj = factory.objectNode();
         ctxtObj.put("ctxt", ctxt);
         try {
-            Files.write(Paths.get(path), ctxtObj.toString().getBytes());
+            Files.write(Paths.get(ctxtFilePath), ctxtObj.toString().getBytes());
         } catch (IOException e) {
-            System.err.println("Could not write ciphertext to file: " + path);
+            System.err.println("Could not write ciphertext to file: " + ctxtFilePath);
         }
+    }
+
+    public static String decryptCtxt(String ctxt) {
+        return SparkFHE.decrypt(ctxt);
     }
 }
