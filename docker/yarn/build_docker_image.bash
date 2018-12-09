3b1202449e37c08ee5e929b242c0cf696889125c
Peizhao Hu <ph@cs.rit.edu>
2018-12-08 20:58:42 -0500

    update the build script

--- docker/yarn/build_docker_image.bash
+++ docker/yarn/build_docker_image.bash
@@ -7,7 +7,7 @@ network_name="hadoop"
 echo -e "Building docker hadoop image\n"
 
 # Remove the relevant image to prevent dangling
-docker rmi $image_name
+[[ -z `docker image ls | grep $image_name` ]] && echo "$image_name does not exist" || docker rmi $image_name
 
 # Create Docker Image
 docker build -t $image_name .
