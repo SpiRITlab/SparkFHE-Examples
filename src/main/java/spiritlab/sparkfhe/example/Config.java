71031436733e0963eac1614317e281da192106bb
Peizhao Hu <ph@cs.rit.edu>
2018-12-22 13:56:47 -0500

    update according to the new reorganization

--- src/main/java/spiritlab/sparkfhe/example/Config.java
+++ src/main/java/spiritlab/sparkfhe/example/Config.java
@@ -2,7 +2,7 @@ package spiritlab.sparkfhe.example;
 
 public class Config {
     public static final String CURRENT_DIRECTORY=System.getProperty("user.dir");
-    public static final String DEFAULT_CRYPTO_PARAMS_FILE=CURRENT_DIRECTORY+"/resources/CRYPTO_PARAMS_HELIB_TEMPLATE.json";
+    public static final String DEFAULT_CRYPTO_PARAMS_FILE=CURRENT_DIRECTORY+"/SparkFHE-Addon/resources/params/CRYPTO_PARAMS_HELIB_TEMPLATE.json";
 
     public static final String DEFAULT_KEY_DIRECTORY=CURRENT_DIRECTORY+"/gen/keys";
     public static final String DEFAULT_PUBLIC_KEY_FILE=DEFAULT_KEY_DIRECTORY+"/my_public_key.txt";
