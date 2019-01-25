9f1c02e13601394359ffa1e53d4422b687d7e64f
Qiaoran Li <qxl8365@rit.edu>
2019-01-25 13:03:25 -0500

    added comments to BasicExample

--- src/main/java/spiritlab/sparkfhe/example/basic/BasicExample.java
+++ src/main/java/spiritlab/sparkfhe/example/basic/BasicExample.java
@@ -8,19 +8,30 @@ import spiritlab.sparkfhe.api.SparkFHE;
 
 import org.apache.spark.SparkConf;
 
+/**
+ * This is an example for SparkFHE project. Created to test the functionality
+ * of the underlying C++ APIs. A few simple functions are invoked via lambda.
+ */
+
 public class BasicExample {
 
-
+    /**
+     * involving the basic C++ API functions
+     */
     public static void test_basic_op() {
+        // testing the addition function
         System.out.println("ADD(1, 0):"+SparkFHE.do_basic_op(1, 0, SparkFHE.ADD));
+        // testing the multiplication function
         System.out.println("MUL(1, 0):"+SparkFHE.do_basic_op(1, 0, SparkFHE.MUL));
+        // testing the substraction function
         System.out.println("SUB(1, 0):"+SparkFHE.do_basic_op(1, 0, SparkFHE.SUB));
     }
 
 
-
     public static void main(String[] args) {
+        // setting up the SparkFHE
         SparkFHESetup.setup();
+        // calling the test_basic_op method
         test_basic_op();
     }
 
