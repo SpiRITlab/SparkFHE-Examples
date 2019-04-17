45a8a8425fb0e68dff2b636c55db28f4b8dbefbc
Qiaoran Li <qxl8365@rit.edu>
2019-04-16 23:48:26 -0400

    refined KeyGenExample.py

--- src/main/python/basic/KeyGenExample.py
+++ src/main/python/basic/KeyGenExample.py
@@ -1,51 +1,141 @@
-##
-# Copyright SpiRITlab - The SparkFHE project.
-# https://github.com/SpiRITlab
-##
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
 
+"""
+Copyright SpiRITlab - The SparkFHE project.
+License_info - https://github.com/SpiRITlab
+"""
+
+# Built-in/Generic Imports
 import os
-cwd = os.getcwd()
 
-param_file_path = "%s/../../../SparkFHE-Addon/resources/params" % cwd
-def generate_param_path(filename):
-    return "%s/%s" % (param_file_path,filename)
-
-key_file_path = "%s/gen/keys" % cwd
-def generate_key_path(filename):
-    return "%s/%s" % (key_file_path,filename)
-
-try:
-    os.mkdir(key_file_path)
-except OSError:
-    print ("Creation of the directory %s failed" % key_file_path)
-else:
-    print ("Successfully created the directory %s " % key_file_path)
-
-
+# Own modules
 from spiritlab import SparkFHE
 
-print("1+0=", SparkFHE.add(1,0))
-print("1*0=", SparkFHE.mul(1,0))
-print("1+0=", SparkFHE.sub(1,0))
-
-fhe = SparkFHE.FHE("HELIB")
-
-
-fhe.generate_key_pair(generate_param_path("CRYPTO_PARAMS_HELIB_TEMPLATE.json"),
-                      generate_key_path("my_public_key.txt"),
-                        generate_key_path("my_secret_key.txt"))
-
-inputNumber="1"
-ptxt = SparkFHE.Plaintext(inputNumber);
-ctxt = fhe.encrypt(ptxt);
-ptxt_result = fhe.decrypt(ctxt)
-
-print "inputNumber=%s, result of dec(enc(inputNumber))=%s" % (inputNumber, ptxt_result.toString())
+__author__ = 'Peizhao Hu'
+__copyright__ = 'Copyright 2019, SparkFHE'
+__credits__ = ['Qiaoran Li']
+__version__ = '0.1.0'
+__maintainer__ = 'SpiRITlab Members'
+__email__ = 'hxpvcs@rit.edu'
+__status__ = '{Dev}'
 
 
+def pretty_title(title):
+    """
+    prints a title to the console before each function for easy reading
+    :param title: a description for the function
+    :return: None
+    """
+    print(title.center(80, '*'))
 
 
+def generate_param_path(param_file_dir, filename):
+    """
+    forms a full path with the param file directory and the filename
+    :param param_file_path: where the param file directory is
+    :param filename: what the filename will be
+    :return: the full param file path
+    """
+    return param_file_dir + '/' + filename
 
+
+def generate_key_path(key_file_dir, filename):
+    """
+    forms a full path with the key file directory and the filename
+    :param key_file_dir: where the key file directory is
+    :param filename: what the filename will be
+    :return: the full key file path
+    """
+    return key_file_dir + '/' + filename
+
+
+def create_key_directory():
+    """
+    creates the directory needed to store the crypto parameters and keys
+    :return: the crypto file directory and key file directory
+    """
+    pretty_title(' Directory Creation ')
+    current_working_dir = os.getcwd()
+    param_file_dir = current_working_dir + '/../../../../SparkFHE-Addon/resources/params'
+    key_file_dir = current_working_dir + '/gen/keys'
+
+    exists = os.path.isdir(key_file_dir)
+    if not exists:
+        try:
+            os.makedirs(key_file_dir)
+        except OSError as e:
+            print ("Creation of the directory %s failed\n" % key_file_dir)
+        else:
+            print ("Successfully created the directory %s\n" % key_file_dir)
+    else:
+        print ("Directory %s already exist\n" % key_file_dir)
+
+    return param_file_dir, key_file_dir
+
+
+def basic_fhe_operation():
+    """
+    test the basic function with in the SparkFHE library
+    :return: None
+    """
+    pretty_title(' Basic Operation ')
+    print("1+0 = %s" %SparkFHE.add(1, 0))
+    print("1*0 = %s" %SparkFHE.mul(1, 0))
+    print("1+0 = %s\n" %SparkFHE.sub(1, 0))
+
+
+def generate_keys(fhe, param_file_path, key_file_path):
+    """
+    test the key generation feature with in the SparkFHE library
+    :param fhe: the SparkFHE object
+    :param param_file_path: where crypto parameter will be stored
+    :param key_file_path: where crypto key will be stored
+    :return: None
+    """
+    pretty_title(' Key Generation ')
+
+    fhe.generate_key_pair(generate_param_path(param_file_path, "CRYPTO_PARAMS_HELIB_TEMPLATE.json"),
+                          generate_key_path(key_file_path, "my_public_key.txt"),
+                          generate_key_path(key_file_path, "my_secret_key.txt"))
+    print('\n')
+
+
+def encrypt_decrypt_validation(fhe, input_number='1'):
+    """
+    test the encrypt and decrypt features of the SparkFHE library
+    :param fhe: the SparkFHE object
+    :param input_number: the plaintext number in string format
+    :return: None
+    """
+    pretty_title(' Encrypt Decrypt Validation ')
+
+    plaintext = SparkFHE.Plaintext(input_number)
+    ciphertext = fhe.encrypt(plaintext)
+    decrypted_plaintext = fhe.decrypt(ciphertext)
+    print ("input_number=%s, result of dec(enc(input_number))=%s\n" % (input_number, decrypted_plaintext.toString()))
+
+
+def main():
+
+    # testing basic Sparkfhe operations
+    basic_fhe_operation()
+
+    # initialize vars and directories
+    param_file_dir, key_file_dir = create_key_directory()
+
+    # creating SparkFHE object with HElib homomorphic encryption
+    fhe = SparkFHE.FHE("HELIB")
+
+    # generating public and secret keys to directories initialized above
+    generate_keys(fhe, param_file_dir, key_file_dir)
+
+    # validate the encrypt and decrypt accuracy with a simple test
+    encrypt_decrypt_validation(fhe)
+
+
+if __name__ == '__main__':
+    main()
 
 
 
